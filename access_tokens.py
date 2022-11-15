1112ace95b2eb09f546e8922b1612571176800f1
bret99 <64787351+bret99@users.noreply.github.com>
2022-11-15 12:47:40 +0000

    Update access_tokens.py

--- access_tokens.py
+++ access_tokens.py
@@ -1,6 +1,6 @@
 censys_api_id = "c"
 censys_secret_key = ""
-criminalip_key = ""
+criminalip_key = "" # 10000 requets per month for free members.
 shodan_key = ""
 zoomeye_key = ""
 nmap_command = "nmap --script vulners -A"
